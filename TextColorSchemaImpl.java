ef1e4caa2910a29a762e50d6dc2ec21042b43763
fomsontret <fomsontret@gmail.com>
2026-02-16 11:14:00 +0300

    Update TextColorSchemaImpl.java

--- TextColorSchemaImpl.java
+++ TextColorSchemaImpl.java
@@ -2,18 +2,17 @@ package ru.netology.graphics.image;
 
 public class TextColorSchemaImpl implements TextColorSchema {
 
-    // Символы от самых темных к самым светлым
+    
     private final char[] SYMBOLS = {'#', '$', '@', '%', '*', '+', '-', '\''};
 
     @Override
     public char convert(int color) {
         // color от 0 (черный) до 255 (белый)
 
-        // Вычисляем индекс в массиве символов
-        // Для черного (0): 0 * 8 / 255 = 0 -> SYMBOLS[0] = '▇'
-        // Для белого (255): 255 * 8 / 255 = 8 -> SYMBOLS[8] = '-'
+        
         int index = color * (SYMBOLS.length - 1) / 255;
 
         return SYMBOLS[index];
     }
+
 }
